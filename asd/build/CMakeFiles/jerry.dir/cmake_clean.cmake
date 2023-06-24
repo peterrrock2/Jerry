42b0fcaeb728451e61f9c8a48d65079da5293cd3
peterrrock2 <peter.r.rock2@gmail.com>
2023-06-24 17:59:32 -0600

    Fist commit

---
+++ asd/build/CMakeFiles/jerry.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jerry.dir/src/kruskal.cpp.o"
+  "CMakeFiles/jerry.dir/src/kruskal.cpp.o.d"
+  "libjerry.pdb"
+  "libjerry.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jerry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
